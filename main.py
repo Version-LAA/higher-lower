b92bb4d79efc5a857ab84ea219232e729aa19d6d
Latoya A <lat.jamison@gmail.com>
2023-12-12 21:15:35 -0500

    created a higher/lower game

---
+++ main.py
@@ -0,0 +1,69 @@
+from art import *
+from game_data import data
+import random
+import os
+
+CELEB_DATA = data
+
+def select_person(arr):
+    # randomly select a person from the array
+    index = random.randint(0,len(arr)-1)
+    return arr[index]
+
+def compare_followers(persona,personb,selection):
+    # checks between two options if user selected the higher option
+    persona_count = persona['follower_count']
+    personb_count = personb['follower_count']
+    a_person = persona
+    b_person = personb
+    if persona_count > personb_count:
+        high = a_person
+    else:
+        high = b_person
+    
+    if selection == high:
+        return True
+    else:
+        return False
+
+
+def play_game(game_data):
+    game_active = True
+    score = 0
+
+    # sets user
+    person_a = select_person(game_data)
+    person_b = select_person(game_data)
+    while person_b == person_a:
+        person_b = select_person(game_data) 
+
+    # while user selects the correct person, the game will continue
+    while game_active:
+        print(f"\nCompare A: {person_a['name']}, a {person_a['description']}, from {person_a['country']}.")
+        print(vs)
+        print(f"\nCompare B: {person_b['name']}, a {person_b['description']}, from {person_b['country']}.")
+        answer = input("\nWho has more followers? Type 'A' or 'B': ")
+        if answer == 'A':
+            selection = person_a
+        else:
+            selection = person_b
+
+        if compare_followers(person_a,person_b,selection):
+            score += 1
+            person_a = selection
+            person_b = select_person(game_data)
+            while person_a == person_b:
+                person_b = select_person(game_data) 
+            os.system("clear")
+        else:
+            print(f"\nSorry you lost! {person_a['name']} has {person_a['follower_count']} vs {person_b['name']} has {person_b['follower_count']}")
+            print(f"Your final score is {score}")
+            game_active = False
+ 
+
+def main():
+    os.system("clear")
+    print(logo)
+    play_game(CELEB_DATA)
+
+main()
